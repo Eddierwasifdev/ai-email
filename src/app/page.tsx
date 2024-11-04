fb4d345cca8a7b991361b9656ea801777929b10f
Eddierwasifdev <phabibon09@gmail.com>
2024-11-04 23:40:05 +0800

    Initial commit

---
+++ src/app/page.tsx
@@ -0,0 +1,10 @@
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
+
+export default async function Home() {
+  return (
+    <div>
+      <Button>hello world</Button>
+    </div>
+  );
+}
